36f7ae1d146778f175b817f1bb6df921e50d610f
Jacob Meacham <jacob.e.meacham@gmail.com>
2015-12-16 23:08:21 -0800

    More improvements.

--- js/main.js
+++ js/main.js
@@ -23,37 +23,6 @@ $(function() {
   function update() {
     ctx.clearRect(0, 0, width*2, height*2);
 
-    var bounds = container.getBoundingClientRect();
-    var posX = (bounds.left + bounds.right) * 0.5;
-    var posY = (bounds.top + bounds.bottom) * 0.5;
-    var radius = (bounds.bottom - bounds.top)*0.5;
-
-    var numCircles = 3;
-    var deltaCircle = 20.0;
-
-    for ( var i = 0; i < numCircles; i+=1 )
-    {
-      var circleRadius = radius + deltaCircle*(numCircles - i) + deltaCircle * 1.5;
-
-      var wobbleRadius = deltaCircle*0.5;
-      var circlePosX = posX + Math.cos(time + i)*wobbleRadius;
-      var circlePosY = posY + Math.sin(time + i)*wobbleRadius;
-      time += dt;
-
-      if ( i == numCircles-1 )
-      {
-        ctx.fillStyle = "#FFCC23";
-        ctx.beginPath();
-        ctx.arc( circlePosX, circlePosY, circleRadius, 0, 2.0 * Math.PI );
-        ctx.fill();
-      }
-
-      ctx.strokeStyle = "white";
-      ctx.beginPath();
-      ctx.arc( circlePosX, circlePosY, circleRadius, 0, 2.0 * Math.PI );
-      ctx.stroke();
-    }
-
     requestAnimFrame(update);
   }
   // shim layer with setTimeout fallback. From
