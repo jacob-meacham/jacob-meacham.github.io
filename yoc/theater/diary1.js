a7088a5684136fa4afda47570fc4aad05d8330a5
Jacob Meacham <jacob.e.meacham@gmail.com>
2017-09-04 16:22:55 -0700

    Updated text, fixed underlines on right diary, other minor modifications.

--- yoc/theater/diary1.js
+++ yoc/theater/diary1.js
@@ -1,84 +1,74 @@
 const diary1Entries = [{
     date: '9-19-2016',
-    text: `Loved the interview with Paula in the paper today. She talked about why she wanted to do Pygmalion instead of My Fair Lady, which everyone knows a little better. She hinted at our dearth of fine singers at the moment (although that didn’t exactly stop Rex Harrison), so we’re getting the pure Shaw treatment. She’s so good at making the case why our little city needs our little community theater program. Stupid city with their stupid budget cuts. Right now it looks like the second weekend of shows is going to be cancelled unless someone drums up a lot of cash. There’s still time. I hope.`
+    text: `Loved the interview with Paula in the paper today. She talked about why she wanted to do Pygmalion instead of My Fair Lady, which everyone knows a little better. She hinted at our dearth of fine singers at the moment (although that didn't exactly stop Rex Harrison), so we're getting the pure Shaw treatment. She's so good at making the case for why our little city needs our little community theater program. Stupid city with their stupid budget cuts. Right now it looks like the second weekend of shows is going to be cancelled unless someone drums up a lot of cash. There's still time. I hope.`
 }, {
     date: '9-20-2016',
-    text: `Dave called today to confirm that I’d volunteer again this year and I practically shouted “of course!” into the phone. I assured him I had BOTH weekends free. I’m staying optimistic about weekend two.
-`
+    text: `Dave called today to confirm that I'd volunteer again this year and I practically shouted "of course!" into the phone. I assured him I had BOTH weekends free. I'm staying optimistic about weekend two.`
 }, {
     date: '9-21-2016',
-    text: `Dave came over from rehearsal at the community center to say hello while I was writing at one of the bakery’s outdoor tables (because what nice weather we’re having, after all). He hinted that he’d been in talks with money. Where did the money come from? Don’t know, but I don’t care as long as it means more shows. I told him to turn on the high school art teacher charm and go get it! On a writing note, my agent sent me another email wondering where the second half of that book is. It’s coming, Devon, it’s coming. I just need a little more time to figure out how to make the gardener look like the guilty party. So then it’s a surprise when the murderer turns out to be the governess.
-`
+    text: `Dave came over from rehearsal at the community center to say hello while I was writing at one of the bakery's outdoor tables (because what nice weather we're having, after all). He hinted that he'd been in talks with money. Where did the money come from? Don't know, but I don't care as long as it means more shows. I told him to turn on the high school art teacher charm and go get it! On a writing note, my agent sent me another email wondering where the second half of that book is. It's coming, Devon, it's coming. I just need a little more time to figure out how to make the gardener look like the guilty party. So then it's a surprise when the murderer turns out to be the governess.`
 }, {
     date: '9-22-2016',
-    text: `But seriously, if I were writing this as a novel, someone would inherit a large amount of money that would insure the company’s productions for years. Problem solved. Of course, there’d also have to be a body or two somewhere. Maybe an ingénue murders her great aunt to get her inheritance. But if she’s a small town ingénue, wouldn’t she just want the money to go to New York rather than saving her smallish city’s community theater? Maybe the ingénue then gets murdered before she can go to New York, and then the money somehow funds the shows. Or the whole company gets murdered one by one, à la Agatha Christie’s Then There Were None. There’d be no show, of course, but I think that could actually make a fine mystery. Hm. I’ll have to sit with that idea for a while.`
+    text: `But seriously, if I were writing this as a novel, someone would inherit a large amount of money that would insure the company's productions for years. Problem solved. Of course, there'd also have to be a body or two somewhere. Maybe an ingénue murders her great aunt to get her inheritance. But if she's a small town ingénue, wouldn't she just want the money to go to New York rather than saving her smallish city's community theater? Maybe the ingénue then gets murdered before she can go to New York, and then the money somehow funds the shows. Or the whole company gets murdered one by one, à la Agatha Christie's Then There Were None. There'd be no show, of course, but I think that could actually make a fine mystery. Hm. I'll have to sit with that idea for a while.`
 }, {
     date: '9-24-2016',
-    text: `I was having lunch and writing at the bakery and the jackhammers from street resurfacing drove me nuts. Next murder weapon, I swear. Or maybe the next victim. I felt so bad for the actors rehearsing next door.
-`
+    text: `I was having lunch and writing at the bakery and the jackhammers from street resurfacing drove me nuts. Next murder weapon, I swear. Or maybe the next victim. I felt so bad for the actors rehearsing next door.`
 }, {
     date: '9-25-2016',
-    text: `Ran into Rich at the farmer’s market today while buying cucumbers (lots of them: today is pickling day). He told me he had a mystery for my next book—Vicky’s dinner went missing at rehearsal yesterday! Turned out to be one of John’s pranks, of course, and she was NOT happy. Paula wasn’t amused either, since it affected their Henry Higgins-Eliza Doolittle chemistry. They’re doing a scene at the library next weekend, and John had better be good until then. Maybe buy her dinner to make up for it? Dinner and a movie? I don’t know whether John’s intentions are entirely pure.
-`
+    text: `Ran into Rich at the farmer's market today while buying cucumbers (lots of them: today is pickling day). He told me he had a mystery for my next book-Vicky's dinner went missing at rehearsal yesterday! Turned out to be one of John's pranks, of course, and she was NOT happy. Paula wasn't amused either, since it affected their Henry Higgins-Eliza Doolittle chemistry. They're doing a scene at the library next weekend, and John had better be good until then. Maybe buy her dinner to make up for it? Dinner and a movie? I don't know whether John's intentions are entirely pure.`
 }, {
     date: '9-26-2016',
     blank: true
 }, {
     date: '9-27-2016',
-    text: `I have a plan. The bakery is going to sponsor the show. They always donate the refreshments for intermission, but this year a little more is in order. I was chatting with Mason, the barista, who said he’d been talking to Sal and Sarah, the owners. Apparently the newspaper article struck a chord (hurrah for the local press!). I’m going to see if I can have a word with them tomorrow. Since I’ve been their best customer since they opened six years ago, I’m hoping I can sway them.
-`
+    text: `I have a plan. The bakery is going to sponsor the show. They always donate the refreshments for intermission, but this year a little more is in order. I was chatting with Mason, the barista, who said he'd been talking to Sal and Sarah, the owners. Apparently the newspaper article struck a chord (hurrah for the local press!). I'm going to see if I can have a word with them tomorrow. Since I've been their best customer since they opened six years ago, I'm hoping I can sway them.`
 }, {
     date: '9-28-2016',
-    text: `Talked to Sal. He was not unwilling to listened to my impassioned plea. He knows how much I love these shows - helping out, watching them take shape each year, and reveling in each performance. I think he really loves it, too. I pointed out that their bakery makes good money from everyone involved, since they rehearse right next door. Paula’s coffee habit alone could keep the bakery in business.
-`
+    text: `Talked to Sal. He was not unwilling to listened to my impassioned plea. He knows how much I love these shows-helping out, watching them take shape each year, and reveling in each performance. I think he really loves it, too. I pointed out that their bakery makes good money from everyone involved, since they rehearse right next door. Paula's coffee habit alone could keep the bakery in business.`
 }, {
     date: '9-29-2016',
-    text: `Talked to Sarah. She says they are “thinking about it.” C’mon, Sarah.`
+    text: `Talked to Sarah. She says they are "thinking about it." C'mon, Sarah.`
 }, {
     date: '10-01-2016',
     blank: true
 }, {
     date: '10-02-2016',
-    text: `I keep running into Rich at the farmer’s market—and I will again next week since they are doing a scene there. He’s talking about tapping his company for a larger sponsorship. Between that and the bakery, we may be saved. (Or, they may be saved. I’m not technically part of their little group.) You’d think that Shelley Frank would be willing to help more since her niece has done tech for so long, but apparently that’s a no-go. Went to the library to watch them perform a scene and hear Paula talk a bit about the play. She encouraged everyone to get their tickets. Ticket sales now will also help keep that second weekend in the theater.
-`
+    text: `I keep running into Rich at the farmer's market-and I will again next week since they are doing a scene there. He's talking about tapping his company for a larger sponsorship. Between that and the bakery, we may be saved. (Or, they may be saved. I'm not technically part of their little group.)
+
+You'd think that Shelley Frank would be willing to help more since her niece has done tech for so long, but apparently that's a no-go. Went to the library to watch them perform a scene and hear Paula talk a bit about the play. She encouraged everyone to get their tickets. Ticket sales now will also help keep that second weekend in the theater.`
 }, {
     date: '10-03-2016',
     blank: true
 }, {
     date: '10-04-2016',
-    text: `Book headway: the gardener is fully implicated and the governess looks innocent (although the children already hated her). End is in sight. I should email Devon an update, but I’ll probably forget. Just as exciting, bakery headway: Sal and Sarah are in! They kindly asked me to deliver the news at rehearsal on Wednesday. Paula already knows, of course, but it will be such a treat to be the one to tell everyone else!
-`
+    text: `Book headway: the gardener is fully implicated and the governess looks innocent (although the children already hated her). End is in sight. I should email Devon an update, but I'll probably forget. Just as exciting, bakery headway: Sal and Sarah are in! They kindly asked me to deliver the news at rehearsal on Wednesday. Paula already knows, of course, but it will be such a treat to be the one to tell everyone else!`
 }, {
     date: '10-05-2016',
     blank: true
 }, {
     date: '10-06-2016',
-    text: `I’ve been more hungover in my life, I guess, but not for a while. Late-night happy hour with the theater people. Paula invited me to come out with them after I told them the happy bakery news. Everyone feels so much more optimistic now, so there was a clearly celebratory air. Theater folk are very, um, loquacious after a drink (or three). Paula pulled out some of her old Broadway stories. Vicky drunk is just as intense as Vicky sober. You get the feeling she could run a small country instead of an office. She is very focused on what she wants. And what she wanted, apparently, was John.
-`
+    text: `I've been more hungover in my life, I guess, but not for a while. Late-night happy hour with the theater people. Michelle invited me to come out with them after I told them the happy bakery news. Everyone feels so much more optimistic now, so there was a clearly celebratory air. Theater folk are very, um, loquacious after a drink (or three). Paula pulled out some of her old Broadway stories. Vicky drunk is just as intense as Vicky sober. You get the feeling she could run a small country instead of an office. She is very focused on what she wants. And what she wanted, apparently, was John.`
 }, {
     date: '10-07-2016',
-    text: `Ran into Dave at the grocery store. He was in a gossipy mood. There’s a little rumor about Vicky and John circulating now. Two thoughts. First: I feel so in the loop! Second: I feel vindicated in both my happy hour observations and guesses after the dinner theft the other week. But oh, John, that dinner-stealing prank was such a fourth grade move.
-`
+    text: `Ran into Dave at the grocery store. He was in a gossipy mood. There's a little rumor about Vicky and John circulating now. Two thoughts. First: I feel so in the loop! Second: I feel vindicated in both my happy hour observations and guesses after the dinner theft the other week. But oh, John, that dinner-stealing prank was such a fourth grade move.`
 }, {
     date: '10-08-2016',
     blank: true
 }, {
     date: '10-09-2016',
-    text: `The scene at the farmer’s market went really well, I thought. I heard that yesterday’s rehearsal was just a mess. Vicky’s been having some, well, issues keeping her accents going, both the Cockney and the fine-lady accent she’s supposed to learn from Higgins. Oh the irony. Today’s scene was better—shows that she’s getting there, really getting there. Just in time. Her ahhhwwwwooo’s sound nice and grating, at least. `
+    text: `The scene at the farmer's market went really well, I thought. I heard that yesterday's rehearsal was just a mess. Vicky's been having some, well, issues keeping her accents going, both the Cockney and the fine-lady accent she's supposed to learn from Higgins. Oh the irony. Today's scene was better-shows that she's getting there, really getting there. Just in time. Her ahhhwwwwooo's sound nice and grating, at least.`
 }, {
     date: '10-10-2016',
-    text: `Had to make a late-night grocery run so I have milk in the morning. I ran into Dave again, and he was very down. One of his set pieces didn’t turn out very well. Poor Dave, now he won’t rest until he perfects it. It’s show week—I guess no one will rest until the whole show is perfect. He did tell me what I’d be doing at the shows, though: I’m going to help set up the lobby during dress rehearsal (which also means I get to watch—yay!) and then I will be in charge of refreshments during the shows. Once Sarah and Sal drop them off, I’ll have charge of them. I’ll do my best not to eat them all…
-`
+    text: `Had to make a late-night grocery run so I have milk in the morning. I ran into Dave again, and he was very down. One of his set pieces didn't turn out very well. Poor Dave, now he won't rest until he perfects it. It's show week-I guess no one will rest until the whole show is perfect. He did tell me what I'd be doing at the shows, though: I'm going to help set up the lobby during dress rehearsal (which also means I get to watch-yay!) and then I will be in charge of refreshments during the shows. Once Sarah and Sal drop them off, I'll have charge of them. I'll do my best not to eat them all...`
 }, {
     date: '10-11-2016',
-    text: `Another mock-menacing email from my agent. Devon, do you not understand how suspenseful this community theater financing business is? This is basically research for my next book. And yet, I told her I’d have the finished book for her by the end of next week. And I think that will actually happen. There may be a couple sleepless nights, but it will happen.
-`
+    text: `Another mock-menacing email from my agent. Devon, do you not understand how suspenseful this community theater financing business is? This is basically research for my next book. And yet, I told her I'd have the finished book for her by the end of next week. And I think that will actually happen. There may be a couple sleepless nights, but it will happen.`
 }, {
     date: '10-12-2016',
-    text: `Paula gave a weird pep talk last night at rehearsal, or so said Rich when I saw him at the bakery this morning. The stress is getting to everybody. Still waiting on word from that final sponsor, so it’s no wonder.`
+    text: `Paula gave a weird pep talk last night at rehearsal, or so said Rich when I saw him at the bakery this morning. The stress is getting to everybody. Still waiting on word from that final sponsor, so it's no wonder.`
 }, {
     date: '10-13-2016',
-    text: `Pygmalion was on TV! Well Paula was, and a little clip of John and Vicky in a scene. Looked really good. Wish I could say the same for dress rehearsal—it was a disaster! Oh my. I was setting up the front of house to get ready for tomorrow, so I only saw parts of it. Major lighting issues meant I really only saw parts of it. No one will like a play in the dark. John forgot a line early on which caused a general ripple effect. Even Connor forgot one of Freddy’s lines, and he doesn’t even have very many. But: bad dress rehearsal, good opening night?`
+    text: `Pygmalion was on TV! Well Paula was, and a little clip of John and Vicky in a scene. Looked really good. Wish I could say the same for dress rehearsal-it was a disaster! Oh my. I was setting up the front of house to get ready for tomorrow, so I only saw parts of it. Major lighting issues meant I really only saw parts of it. No one will like a play in the dark. John forgot a line early on which caused a general ripple effect. Even Connor forgot one of Freddy's lines, and he doesn't even have very many. But: bad dress rehearsal, good opening night?`
 }, {
     date: '10-14-2016',
     text: `Bad dress rehearsal, good opening night!`
